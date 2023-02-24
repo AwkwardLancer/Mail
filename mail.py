a3423477ddba487ae47a9c4ecd2d07b8cb836791
AwkwardLancer <110264661+AwkwardLancer@users.noreply.github.com>
2023-02-24 22:57:53 +0000

    Update mail.py

--- mail.py
+++ mail.py
@@ -8,7 +8,7 @@ def Main():
 	print("")
 	print("Thank you for using mail :) ")
 	print("")
-	print("Instructions: use the temporary email in any mailbox, then, if you receive an email, you will receive a link containing the message's content, which can be seen in the terminal by running this command in another shell:curl -s '<url>' | jq")
+	print("Instructions: use the temporary email in any mailbox, then, if you receive an email, you will receive a link containing the message's content.")
 	print("")
 	print("Here is your temporary email, if you want another email, please close the program and run it again!")
 	print("")
